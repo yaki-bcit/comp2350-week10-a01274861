b18091b854cb157d9997ddd4ea5d89d6660fb1df
Yevgeniy Akimenko <yakimenko@bcit.ca>
2022-03-13 12:30:11 -0700

    initial commit

---
+++ routes/router.js
@@ -0,0 +1,102 @@
+const router = require('express').Router();
+const database = include('databaseConnection');
+const dbModel = include('databaseAccessLayer');
+//const dbModel = include('staticData');
+
+router.get('/', (req, res) => {
+	console.log("page hit");
+	database.getConnection(function (err, dbConnection) {
+		if (err) {
+			res.render('error', {message: 'Error connecting to MySQL'});
+			console.log("Error connecting to mysql");
+			console.log(err);
+		}
+		else {
+			
+			dbModel.getAllUsers((err, result) => {
+				if (err) {
+					res.render('error', {message: 'Error reading from MySQL'});
+					console.log("Error reading from mysql");
+					console.log(err);
+				}
+				else { //success
+					res.render('index', {allUsers: result});
+
+					//Output the results of the query to the Heroku Logs
+					console.log(result);
+				}
+			});
+			dbConnection.release();
+		}
+	});
+
+});
+
+router.post('/addUser', (req, res) => {
+	console.log("form submit");
+	database.getConnection(function (err, dbConnection) {
+		if (err) {
+			res.render('error', {message: 'Error connecting to MySQL'});
+			console.log("Error connecting to mysql");
+			console.log(err);
+		}
+		else {
+			console.log(req.body);
+			dbModel.addUser(req.body, (err, result) => {
+				if (err) {
+					res.render('error', {message: 'Error writing to MySQL'});
+					console.log("Error writing to mysql");
+					console.log(err);
+				}
+				else { //success
+					res.redirect("/");
+
+					//Output the results of the query to the Heroku Logs
+					console.log(result);
+				}
+			});
+			
+			dbConnection.release();
+		}
+	});
+
+});
+
+router.get('/deleteUser', (req, res) => {
+	console.log("delete user");
+	database.getConnection(function (err, dbConnection) {
+		if (err) {
+			res.render('error', {message: 'Error connecting to MySQL'});
+			console.log("Error connecting to mysql");
+			console.log(err);
+		}
+		else {
+			console.log(req.query);
+
+			let userId = req.query.id;
+			if (userId) {
+				dbModel.deleteUser(userId, (err, result) => {
+					if (err) {
+						res.render('error', {message: 'Error writing to MySQL'});
+						console.log("Error writing to mysql");
+						console.log(err);
+					}
+					else { //success
+						res.redirect("/");
+
+						//Output the results of the query to the Heroku Logs
+						console.log(result);
+					}
+				});
+			}
+			else {
+				res.render('error', {message: 'Error on Delete'});
+			}
+		
+			dbConnection.release();
+		}
+	});
+});
+
+
+module.exports = router;
